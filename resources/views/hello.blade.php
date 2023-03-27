0be474dab3809a0cd16c5b3bbfc1cae2eb9af45a
Rajat <rjtthwl@gmail.com>
2023-03-27 11:57:01 +0530

    routing and components

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,3 @@
+<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, fuga eveniet recusandae est, ea praesentium reprehenderit amet veritatis iste saepe odit unde in doloremque velit! Modi repudiandae voluptatibus quis fugiat, vitae enim placeat, illum quo excepturi, quisquam quibusdam quidem quas. Modi autem dolorum officia vitae dolor asperiores iusto minus excepturi.</h1>
+<a href="/about">About</a>
+<a href="/contact">Contact</a>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('hello');
 });
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::view("users","users")
