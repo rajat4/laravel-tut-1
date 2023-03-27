24730b5d6d683477f4f28c45a7bd6b543e12490e
Rajat <rjtthwl@gmail.com>
2023-03-27 18:32:49 +0530

    lorem10

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -8,4 +8,6 @@
       illum quo excepturi, quisquam quibusdam quidem quas. 
       Modi autem dolorum officia vitae dolor asperiores iusto minus excepturi.</h1>
 
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, eveniet!</h1>
+
 <x-footer/>
