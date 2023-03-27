52a045458c2c421ec93d00d7b26d7838a3f57d63
Rajat <rjtthwl@gmail.com>
2023-03-27 16:52:46 +0530

    header and footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
+    <a href="/">Home</a>
+<a href="/about">About</a>
+<a href="/contact">Contact</a>
+<a href="/users">Users</a>
+<br>
+{{URL::current()}}
+<br>
+{{URL::full()}}
+<br>
+{{URL::previous()}}
+</div>
--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,11 +1,5 @@
 <x-header componentName="Hello"/>
-<head>
-    <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" 
-href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" 
-integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
-crossorigin="anonymous">
-</head>
+@include('cdn')
 
 <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
     Illo, fuga eveniet recusandae est, ea praesentium reprehenderit
@@ -14,13 +8,4 @@ crossorigin="anonymous">
       illum quo excepturi, quisquam quibusdam quidem quas. 
       Modi autem dolorum officia vitae dolor asperiores iusto minus excepturi.</h1>
 
-<a href="/">Home</a>
-<a href="/about">About</a>
-<a href="/contact">Contact</a>
-<a href="/users">Users</a>
-<br>
-{{URL::current()}}
-<br>
-{{URL::full()}}
-<br>
-{{URL::previous()}}
+<x-footer/>
