d7e1fba57c116a666d41ffd8574fe27b29d5cade
Rajat <rjtthwl@gmail.com>
2023-03-27 16:04:04 +0530

    URL generation and bootstrap

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,4 +1,12 @@
 <x-header componentName="Hello"/>
+<head>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" 
+href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" 
+integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
+crossorigin="anonymous">
+</head>
+
 <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
     Illo, fuga eveniet recusandae est, ea praesentium reprehenderit
      amet veritatis iste saepe odit unde in doloremque velit! 
@@ -10,3 +18,9 @@
 <a href="/about">About</a>
 <a href="/contact">Contact</a>
 <a href="/users">Users</a>
+<br>
+{{URL::current()}}
+<br>
+{{URL::full()}}
+<br>
+{{URL::previous()}}
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,6 +1,19 @@
 <x-header componentName="Users" />
+<head>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" 
+href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" 
+integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
+crossorigin="anonymous">
+</head>
 <h1>User Page</h1>
 <a href="/">Home</a>
 <a href="/about">About</a>
 <a href="/contact">Contact</a>
 <a href="/users">Users</a>
+<br>
+{{URL::current()}}
+<br>
+{{URL::full()}}
+<br>
+{{URL::previous()}}
